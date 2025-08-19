b63432a3b91c2813626b0322fdadbd470e59cac5
Antony D. <115439258+kazekeshx@users.noreply.github.com>
2025-08-19 15:42:10 -0500

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,21 +1,6 @@
 import { CorsOptions } from "cors";
 
-const whitelist = [
-    process.env.FRONTEND_URL,
-    "http://localhost:4000",
-    "https://nebacperu.com",
-    "https://nebacperu.com/inventario",
-    "https://www.nebacperu.com",
-    "https://www.nebacperu.com/inventario"
-];
-
 export const corsConfig: CorsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || whitelist.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Error de CORS"));
-        }
-    },
+    origin: true,  // Permite cualquier origen
     credentials: true,
 };
